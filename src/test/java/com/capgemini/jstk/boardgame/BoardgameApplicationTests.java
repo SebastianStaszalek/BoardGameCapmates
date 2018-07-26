4b235055b858caf52f73f61e5a8c084a80486693
SSTASZAL <SSTASZAL@CE24419.corp.capgemini.com>
2018-07-26 16:04:33 +0200

    new tests created for UserProfileService

--- src/test/java/com/capgemini/jstk/boardgame/BoardgameApplicationTests.java
+++ src/test/java/com/capgemini/jstk/boardgame/BoardgameApplicationTests.java
@@ -1,16 +1,39 @@
 package com.capgemini.jstk.boardgame;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.capgemini.jstk.boardgame.dto.UserTO;
+import com.capgemini.jstk.boardgame.service.UserProfileService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BoardgameApplicationTests {
+	
+	@Autowired
+	UserProfileService userProfileService;
 
 	@Test
-	public void contextLoads() {
+	public void shouldUpdateUserProfile() {
+		//given
+		UserTO userToUpdate = UserTO.builder()
+				.eMail("test@wp.pl")
+				.password("newPass")
+				.firstName("Ola")
+				.lastName("Obroszko")
+				.build();
+		//when
+		userProfileService.update(userToUpdate);
+	
+		UserTO usertTOCheck = userProfileService.getProfileInformation("test@wp.pl");
+		//then
+		assertEquals("newPass", usertTOCheck.getPassword());
+		assertEquals(null, usertTOCheck.getMotto());
 	}
 
 }
--- src/test/java/com/capgemini/jstk/boardgame/service/UserProfileServiceTest.java
+++ src/test/java/com/capgemini/jstk/boardgame/service/UserProfileServiceTest.java
@@ -1,22 +1,99 @@
 package com.capgemini.jstk.boardgame.service;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.capgemini.jstk.boardgame.domain.UserEntity;
+import com.capgemini.jstk.boardgame.dto.UserTO;
+import com.capgemini.jstk.boardgame.mapper.UserMapper;
 import com.capgemini.jstk.boardgame.repository.UserRepository;
+import com.capgemini.jstk.boardgame.service.implementation.UserProfileServiceImp;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserProfileServiceTest {
 	
+	@Spy
+	UserMapper userMapper;
+	
 	@Mock
 	UserRepository userRepository;
 	
 	@InjectMocks
-	UserProfileService userProfileService;
+	UserProfileServiceImp userProfileService;
+	
+	@Test 
+	public void shouldReturnUserProfileInformation() {
+		//given
+		UserEntity user = UserEntity.builder()
+				.eMail("jadzia@interia.pl")
+				.firstName("Jadzia")
+				.lastName("Dobrowolska")
+				.password("jadziaxxx")
+				.build();
+		
+		Mockito.when(userRepository.getUserByEMail(Mockito.anyString())).thenReturn(user);
+		
+		//when
+		userProfileService.getProfileInformation("jadzia@interia.pl");
+		//then
+		Mockito.verify(userRepository).getUserByEMail(Mockito.anyString());
+	}
+	
+	@Test
+	public void shouldUpdateUserProfile() {
+		//given
+		UserEntity user = UserEntity.builder()
+				.eMail("jadzia@interia.pl")
+				.firstName("Jadzia")
+				.lastName("Dobrowolska")
+				.password("jadziaxxx")
+				.build();
+		
+		UserTO userTO = UserTO.builder()
+				.eMail("jadzia@interia.pl")
+				.firstName("Jadzia")
+				.lastName("Dobrowolska")
+				.password("jadziaxxx")
+				.build();
+		
+		
+		Mockito.when(userRepository.update(Mockito.any(UserEntity.class))).thenReturn(user);
+		//when
+		userProfileService.update(userTO);
+		//then
+		Mockito.verify(userRepository).update(Mockito.any(UserEntity.class));
+	}
+	
+	@Test
+	public void shouldCreateUserProfile() {
+		//given
+		UserEntity user = UserEntity.builder()
+				.eMail("madzix@wp.pl")
+				.firstName("Madzia")
+				.lastName("Janik")
+				.password("mmm")
+				.build();
+		
+		UserTO userTO = UserTO.builder()
+				.eMail("madzix@wp.pl")
+				.firstName("Madzia")
+				.lastName("Janik")
+				.password("mmm")
+				.build();
+		
+		Mockito.when(userRepository.update(Mockito.any(UserEntity.class))).thenReturn(user);
+		//when
+		userProfileService.createUserProfile(userTO);
+		//then
+		Mockito.verify(userRepository).createUser(Mockito.any(UserEntity.class));
+	}
 	
 	
 }
